fc19a34782bdfdd40947cc182249160d6a0e75be
audreysoucie <audreysoucie@weber.edu>
2024-03-31 20:48:40 -0600

    incomplete

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,16 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+function addNewName(event){
+    let f = document.querySelector("#fname").value
+    let l = document.querySelector("lname").value
+
+    let names = getName()
+    if(f && l){
+        let name = { fname: f, lname: l}
+        name.push(name)
+        localStorage.setItem('names', JSON.stringify (names))
+    }
+    
+    this.reset
+}   
